08e8f4bfbcaebe36f59ce96c550add31206e6e57
albertpiacka <albertpiacka@gmail.com>
2021-02-14 10:04:03 +0100

    removed addtocart from allmixins (might return it later)

--- src/mixins/AllMixins.js
+++ src/mixins/AllMixins.js
@@ -1,27 +1,5 @@
 export default {
     methods: {
-        addToCart(item){
-            var existing = localStorage.getItem('basket')
-
-            existing = existing ? existing.split(',') : []
-
-            let obj = {
-                id: (Math.random()*1000).toFixed(0),
-                name: item.name,
-                quantity: 1,
-                package: `${item.selected * 1000}gr`,
-                price: ((item.price * Number(item.selected)) * 1).toFixed(2)
-            }
-
-            existing.push(JSON.stringify([obj]));
-
-            localStorage.setItem('basket', existing.toString())
-
-            item.selected = '0.25'
-
-            this.$root.$emit('flash', 'Item added to cart')
-        },
-
         setPrice(item){
             item.selectedPrice = item.price * item.selected
 
